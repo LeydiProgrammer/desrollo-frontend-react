8d910074aa166b7af84622c1685834ed05d3c268
LeydiProgrammer <ineltenil1212@gmail.com>
2025-01-15 19:25:29 -0400

    practica

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
       <BrowserRouter>
         <App />
       </BrowserRouter>
--- src/screens/Product.jsx
+++ src/screens/Product.jsx
@@ -1,11 +1,18 @@
 
+import FilterableProductTable from '../Components/Product/FilterableProductTable'
+
+
+
+import {useSelector} from 'react-redux'
 
 const Product = () => {
+ 
+  const dataProduct = useSelector((state) => state.product.dataProduct);
   return (
     <>
-        <p>Componente Product</p>
-        </>
+        <FilterableProductTable products={dataProduct} />
+    </>
   )
-}
+};
 
-export default Product
+export default Product;
